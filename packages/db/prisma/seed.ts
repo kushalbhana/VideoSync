90e26792915c63b53d30a7e71a2a212446792cc3
kushalbhana <68599557+kushalbhana@users.noreply.github.com>
2024-04-28 01:47:54 +0530

    Database setup and tailwind initialized

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { number: '1111111111' },
+    update: {},
+    create: {
+      number: '1111111111',
+      password: '123456',
+      name: 'alice',
+      
+    },
+  })
+}
